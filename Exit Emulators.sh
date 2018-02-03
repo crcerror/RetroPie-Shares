eaa618e5273af18604dc00b97712d557a590015b
cyperghost <crcerror@users.noreply.github.com>
2018-02-03 22:53:16 +0100

    Annother way without RegEx

---
+++ Exit Emulators.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+function getcpid() {
+local cpids="$(pgrep -P $1)"
+    for cpid in $cpids;
+    do
+        pidarray+=($cpid)
+        getcpid $cpid
+    done
+}
+
+smart_wait() {
+    local PID=$1
+    while [[ -e /proc/$PID ]]
+    do
+        sleep 0.05
+    done
+}
+
+
+motherpid="$(pgrep -f -n runcommand.sh)"
+[ "$motherpid" ] && getcpid $motherpid || exit
+for ((z=${#pidarray[*]}-1; z>-1; z--))
+do
+ kill ${pidarray[z]} && smart_wait ${pidarray[z]}
+done
