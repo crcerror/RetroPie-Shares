1efe618145cd8de7685b0b54937820f289f5dde0
cyperghost <crcerror@users.noreply.github.com>
2018-06-30 20:31:19 +0200

    Update OpenBOR - Beats of Rage Engine Selection.sh

--- OpenBOR - Beats of Rage Engine Selection.sh
+++ OpenBOR - Beats of Rage Engine Selection.sh
@@ -1,23 +1,30 @@
-#!/bin/bash
+!/bin/bash
 # Graphical Menu to select OpenBOR custom files
-# Thank to darknior for working on OpenBOR
-# Follow hin @chrono
+# Thank to @darknior for working on OpenBOR on RetroPie forum
+# Follow him @chronocrash official forum
 # For setup read more here
 # https://retropie.org.uk/forum/topic/13784
 #
 # I'm using @darkniors file nameing convention so every game episode uses
 # gameepisode.bor/data directory structure
 #
-# The purposes? To make use of runcommand.sh for better compatibility to RetroPie setups
+# The purposes?
+# Reason 1: To make use of runcommand.sh for better compatibility to RetroPie setups
+# Reason 2: To automate setup of joypad for each game
+#             > copy a cfg file to $BORBASE_DIR and name it master.bor.cfg
+#             > so a once done setup file must be copied from $KEYCONF_DIR to $BORBASE_DIR
+#
 # coded by cyperghost
 # For https://retropie.org.uk/
 
 ###### --------------------- INIT ---------------------
-readonly VERSION="0.9_063018"
+readonly VERSION="0.95_063018"
 readonly TITLE="OpenBOR - cyperghosts Episode selector"
 readonly BORBASE_DIR="/home/pi/RetroPie/roms/ports/openbor"
 readonly ROOTDIR="/opt/retropie"
+readonly KEYCONF_DIR="$ROOTDIR/configs/ports/openbor/Saves"
 ###### --------------------- INIT ---------------------
+
 
 
 
@@ -31,7 +38,6 @@ function start_joy(){
         # see: http://pubs.opengroup.org/onlinepubs/7908799/xcurses/terminfo.html
         "$ROOTDIR/supplementary/runcommand/joy2key.py" "/dev/input/js0" kcub1 kcuf1 kcuu1 kcud1 0x0a 0x09 &
         JOY2KEY_PID=$!
-
 }
 
 function end_joy(){
@@ -43,7 +49,6 @@ function end_joy(){
         kill -INT "$JOY2KEY_PID"
     fi
 
-    sleep 1
 }
 
 ###### --------------- Dialog Functions ---------------
@@ -53,11 +58,12 @@ function end_joy(){
 
 function dialog_error() {
     dialog --title " Error " --backtitle " $TITLE - $VERSION " --infobox "$1" 0 0
-}
+    sleep "$2"; clear
+	}
 
 # This builds dialog for OpenBOR directories
 # We need to create valid array (dialog_array) before
-# I disabled tags, so selections are showen exactly as in ROMs
+# I disabled tags, so selections are showen exactly as in ES ROM section
 
 function dialog_selectBOR() {
 
@@ -124,16 +130,14 @@ if [[ -d $BORBASE_DIR ]]; then
     cd "$BORBASE_DIR"
     bor_files=$(find -maxdepth 1 -iname "*.bor" -type d 2>/dev/null)
 else
-    dialog_error "No $BORBASE_DIR found!"
-    sleep 3
+    dialog_error "No $BORBASE_DIR found!" "3"
     exit
 fi
 
 # Are there any BOR Directories available?
 
 if [[ -z $bor_files ]]; then
-    dialog_error "No BOR directories found in location:\n$BORBASE_DIR"
-    sleep 3
+    dialog_error "No BOR directories found in location:\n$BORBASE_DIR" "3"
     exit
 fi
 
@@ -143,16 +147,25 @@ start_joy
 
 build_find_array "$bor_files"
 BOR_file=$("dialog_selectBOR")
-
-end_joy
+clear
+end_joy; sleep 0.5
 
 if [[ -z $BOR_file ]]; then
-    dialog_error "No Selection made ... going back to ES!"
-    sleep 1
+    dialog_error "No Selection made ... Returning to ES!" "2"
     exit
+fi
+
+# Is there a config file (I don't want to setup each file one by one)
+# If not I use the Masterfile which I copied to $BORBASE_DIR
+# master file is names master.bor.cfg
+# I use a good configuration file from a older setup to copy key settings for new games ;)
+BOR_cfg="$KEYCONF_DIR${BOR_file#.*}.cfg"
+if [[ ! -f $BOR_cfg && -f $BORBASE_DIR/master.bor.cfg  ]]; then
+    cp "$BORBASE_DIR/master.bor.cfg" "$BOR_cfg"
+    dialog_error "Copied config-file from:\n$BORBASE_DIR/master.bor.cfg\n    to:\n$BOR_cfg\n\nStarting game ${BOR_file:2:-4} in a few seconds!" "8"
 fi
 
 # Finally using RUNCOMMAND.SH to initiate prober start of games
 
 BOR_file="$BORBASE_DIR${BOR_file#.*}"
-"/opt/retropie/supplementary/runcommand/runcommand.sh" 0 _PORT_ "openbor" "$BOR_file"
+"$ROOTDIR/supplementary/runcommand/runcommand.sh" 0 _PORT_ "openbor" "$BOR_file"
