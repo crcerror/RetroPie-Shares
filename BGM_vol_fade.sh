d6f6f4f2a5063cc4bb53c22194a941e58bb084f1
cyperghost <crcerror@users.noreply.github.com>
2018-11-02 18:15:20 +0100

    Update BGM_vol_fade.sh

--- BGM_vol_fade.sh
+++ BGM_vol_fade.sh
@@ -2,15 +2,20 @@
 
 # A script to fade ALSA volume when starting or ending
 # a Emulator by using amixer and MASTER/PCM Control.
+#
 # kill -19 pauses musicplayer, kill -18 continues musicplayer
+#
 # Place the script in runcommand-onstart.sh to fade-out
 # Place the script in runcommand-onend.sh to fade-in
+# For the onend script I recommend a line like this: 
+# (sleep 2 && $HOME/RetroPie/scripts/vol_fade.sh) &
+#
 # Status of musicplayer is determinated automatically
 #
 # by cyperghost
-# 2018/10/31 - Halloween
+# 2018/11/01 - All saints' Day
 
-# Reason I like the pyscript for BGM but it has it flaws and caveeats
+# Reason: I like the pyscript for BGM but it has it flaws and caveeats
 # so I recommend the mpg123 method brought by synack
 # Read here how to setup https://retropie.org.uk/forum/topic/9133
 
@@ -18,11 +23,17 @@
 readonly VOLUMECHANNEL="PCM"
 readonly MUSICPLAYER="mpg123"
 
+# Avoid multiple starts, so force wait till only one script is running
+# This avoids overriding of volumes and reverse of fade-in and fade-out
+while [[ "$(pgrep -c -f $(basename $0))" -gt 1 ]]; do
+    sleep 1
+done
+
 # Get ALSA volume value and calculate step
 VOLUMEALSA=$(amixer -M get $VOLUMECHANNEL | grep -o "...%]")
 VOLUMEALSA=${VOLUMEALSA//[^[:alnum:].]/}
-VOLUMESTEP=$(echo "scale=1; $VOLUMEALSA/20" | bc -l) # Volume with float
-#VOLUMESTEP=$(($VOLUMEALSA/20)) # Old version this will calculate only integer
+FADEVOLUME=
+VOLUMESTEP=
 
 # ALSA-Commands
 VOLUMEZERO="amixer -q -M set $VOLUMECHANNEL 0%"
@@ -32,9 +43,21 @@ VOLUMERESET="amixer -q -M set $VOLUMECHANNEL $VOLUMEALSA%"
 PLAYERPID="$(pidof $MUSICPLAYER)"
 PLAYERSTATUS=$(ps -ostate= -p $PLAYERPID)
 
+function set_step() {
+    case $FADEVOLUME in
+        [1-4][0-9]|40) VOLUMESTEP=5 ;;
+        [5-7][0-9]|80) VOLUMESTEP=3 ;;
+        [8-9][0-9]|100) VOLUMESTEP=1 ;;
+        *) VOLUMESTEP=5 ;;
+     esac
+}
+
 if [[ $PLAYERSTATUS == *S* ]]; then
     # Fading down and pausing in twenty steps
-    for a in {0..19}; do
+    FADEVOLUME=$VOLUMEALSA
+    until [[ $FADEVOLUME -le 10 ]]; do
+        set_step
+        FADEVOLUME=$(($FADEVOLUME-$VOLUMESTEP))
         amixer -q -M set "$VOLUMECHANNEL" "${VOLUMESTEP}%-"
         sleep 0.1
     done
@@ -45,16 +68,20 @@ if [[ $PLAYERSTATUS == *S* ]]; then
     $VOLUMERESET
 
 elif [[ $PLAYERSTATUS == *T* ]]; then
-    # Playing and fading up in ten steps
+    # Playing and fading in
     $VOLUMEZERO
     sleep 0.5
     kill -18 $PLAYERPID
-    for a in {0..19}; do
-        [[ $a -lt 13 ]] && sleep 0.1
-        [[ $a -gt 12 ]] && sleep 0.25
+
+    FADEVOLUME=10
+    until [[ $FADEVOLUME -ge $VOLUMEALSA ]]; do
+        set_step
+        FADEVOLUME=$(($FADEVOLUME+$VOLUMESTEP))
         amixer -q -M set "$VOLUMECHANNEL" "${VOLUMESTEP}%+"
+        sleep 0.2
     done
     $VOLUMERESET
+
 else
     echo "Musicplayer: $MUSICPLAYER is not running."
 fi
