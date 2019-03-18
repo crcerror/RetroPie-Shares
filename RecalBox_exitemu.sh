73375f97e345fae8d5dd817819e441c6f456cdc8
cyperghost <crcerror@users.noreply.github.com>
2019-03-18 21:10:31 +0100

    Productive version
    Full command set
    help added

--- RecalBox_exitemu.sh
+++ RecalBox_exitemu.sh
@@ -1,26 +1,79 @@
 #!/bin/bash
-# Script to exit emulators used by Recalbox
+#
+# Script for RecalBox to terminate every emulator instance
+# Control script to give feedback about state of EmulationStation and
+# active EMULATORS
+# by cyperghost aka crcerror // 18.03.2019
+# 
+
+# Get all childpids from calling process
 function getcpid() {
 local cpids="$(pgrep -P $1)"
-    for cpid in $cpids;
-    do
+    for cpid in $cpids; do
         pidarray+=($cpid)
         getcpid $cpid
     done
 }
 
-smart_wait() {
+# Get a sleep while process is active in background
+function smart_wait() {
     local PID=$1
-    while [[ -e /proc/$PID ]]
-    do
+    while [[ -e /proc/$PID ]]; do
         sleep 0.25
     done
 }
 
+# Emulator currently running?
+function check_emurun() {
+    local RC_PID="$(pgrep -f -n emulatorlauncher)"
+    echo $RC_PID
+}
 
-motherpid="$(pgrep -f -n emulatorlauncher.py)"
-[ "$motherpid" ] && getcpid $motherpid || exit
-for ((z=${#pidarray[*]}-1; z>-1; z--))
-do
- kill ${pidarray[z]} && smart_wait ${pidarray[z]}
-done
+# Emulationstation currently running?
+function check_esrun() {
+    local ES_PID="$(pidof emulationstation)"
+    echo $ES_PID
+}
+
+# ---- MAINS ----
+
+case ${1,,} in
+    --restart)
+        echo "Restarting now ..."
+        /etc/init.d/S31emulationstation start 
+    ;;
+
+    --espid)
+        # Display ES PID to stdout
+        ES_PID=$(check_esrun)
+        [[ -n $ES_PID ]] && echo $ES_PID || echo 0
+    ;;
+
+    --emupid)
+        # This helps to detect emulator is running or not
+        RC_PID=$(check_emurun)
+        [[ -n $RC_PID ]] && echo $RC_PID || echo 0
+    ;;
+
+    --emukill)
+        RC_PID=$(check_emurun)
+        if [[ -n $RC_PID ]]; then
+            getcpid $RC_PID
+            for ((z=${#pidarray[*]}-1; z>-1; z--)); do
+                kill ${pidarray[z]}
+                smart_wait ${pidarray[z]}
+            done
+            unset pidarray
+        fi
+    ;;
+
+    *)
+        echo -e "Please parse parameters to this script! \n 
+                  --restart will RESTART EmulationStation only \n
+                  --shutdown will SHUTDOWN whole system \n
+                  --emukill to exit any running EMULATORS \n
+                  --espid to check if EmulationStation is currently active \n
+                  --emupid to check if an Emulator is running"
+    ;;
+
+esac
